1be1e01fdf5c6620daa13a21f0e0e3a7396eb8ee
johnguo <john.guo@statcan.gc.ca>
2025-01-29 10:54:50 -0500

    Added some comments and chooseDrone method

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -2,25 +2,44 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Scheduler extends Thread{
-    Drone drone;
-    FireIncidentSubsystem fireIncidentSubsystem;
-    HashMap<Integer,HashMap<String, String>> zone;
+    private Drone drone; //Only one drone for iteration 1
+    private FireIncidentSubsystem fireIncidentSubsystem;
+    private HashMap<Integer,HashMap<String, String>> zone;
 
+    /**
+     * Constructor for new Scheduler object
+     *
+     * @param fireIncidentSubsystem
+     */
     public Scheduler(FireIncidentSubsystem fireIncidentSubsystem){
         this.fireIncidentSubsystem = fireIncidentSubsystem;
     }
 
-    public void reportFire(HashMap<Integer,HashMap<String, String>> zone){
+    public void newFireRequest(HashMap<Integer,HashMap<String, String>> zone){
         //TODO
         dispatchDrone();
     }
 
     private void dispatchDrone(){
+        Drone drone = chooseDrone();
         drone.someFunction(); //TODO
     }
 
+    /**
+     * Run by the Scheduler to choose which drone will be sent to the fire zone
+     *
+     * @return the drone that will be sent to the fire zone
+     */
+    private Drone chooseDrone(){
+        return drone;
+    }
+
+    /**
+     * Sets the drone for the Scheduler
+     *
+     * @param drone the drone the Scheduler will use to send to fires zones
+     */
     public void addDrone(Drone drone){
-        //Will need to be redone later
         this.drone = drone;
     }
 }

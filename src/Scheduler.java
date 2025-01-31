92a986f7a15fe11ff8fc1fd9265c587a9cec2dd5
johnguo471471 <jguo471@gmail.com>
2025-01-30 21:00:29 -0500

    Redid comments (will need to be redone later)

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -9,30 +9,55 @@ public class Scheduler extends Thread{
     private final int MODERATE = 20;
     private final int HIGH = 30;
 
+    /**
+     * Constructor for Scheduler class
+     *
+     * @param fireIncident the FireIncident subsystem that the Scheduler will send messages to
+     */
     public Scheduler(FireIncident fireIncident){
         this.fireIncident = fireIncident;
         this.drones = new HashMap<Integer, Drone>();
     }
 
+    /**
+     * Scheduler receives a fire request and runs dispatchDrone method
+     *
+     * @param zone the fire zone passed by the FireIncident subsystem
+     */
     public void newFireRequest(HashMap<Integer,HashMap<String, String>> zone){
-        //TODO
+        //TODO input parameters may need to be changed
         dispatchDrone(zone);
     }
 
+    /**
+     * Scheduler receives data from drone which is then sent back to the FireIncident subsystem
+     *
+     * @param zone the fire zone passed by the Drone
+     */
     public void droneReturn(HashMap<Integer,HashMap<String, String>> zone){
-        this.fireIncident.put(zone); //TODO signal the fireIncident that drone has returned
+        //TODO input parameters may need to be changed
+        //TODO FireIncident method needs to be added
+        this.fireIncident.put(zone);
     }
 
-    private Boolean dispatchDrone(HashMap<Integer,HashMap<String, String>> zone){
+    /**
+     * Scheduler runs chooseDrone method runs then sends the fire zone information to the drone
+     *
+     * @param zone the fire zone passed from the FireIncident
+     */
+    private void dispatchDrone(HashMap<Integer,HashMap<String, String>> zone){
         Drone chosen_drone = chooseDrone();
         if (chosen_drone == null){
             System.out.print("There is no drone to send");
-            return false;
         }
-        chosen_drone.send(); //TODO signal the drone
-        return true;
+        chosen_drone.send(); //TODO Drone method needs be added
     }
 
+    /**
+     * Scheduler chooses a Drone out of the drone list
+     *
+     * @return the Drone chosen
+     */
     private Drone chooseDrone(){
         //Chooses the first drone
         for (Drone drone: this.drones.values()){
@@ -41,6 +66,12 @@ public class Scheduler extends Thread{
         return null;
     }
 
+    /**
+     * Adds a Drone to the drones hashmap
+     *
+     * @param droneID the ID of the Drone
+     * @param drone the Drone object
+     */
     public void addDrone(Integer droneID, Drone drone){
         this.drones.put(droneID, drone);
     }

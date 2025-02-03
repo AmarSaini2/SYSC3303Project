5a1630c3cb41303fa82a9bcf18e984ffd32c0dcf
amard <amardsaini2@gmail.com>
2025-02-02 20:31:39 -0500

    Added JAR file

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Scheduler scheduler = new Scheduler();
         FireIncident fireIncident = new FireIncident("src\\Event_File.csv", "src\\Zone_File.csv", scheduler);
+        //FireIncident fireIncident = new FireIncident("src\\Event_File.csv", "src\\Zone_File.csv", scheduler);
         Drone drone = new Drone(scheduler);
         scheduler.start();
         fireIncident.start();

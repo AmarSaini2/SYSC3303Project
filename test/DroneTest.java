de3f4cc7dde636bd2035c5a146860dc48fbf7f73
John Guo <john471g@gmail.com>
2025-03-15 17:13:59 -0400

    Added drone test that goes through the drone run method

--- test/DroneTest.java
+++ test/DroneTest.java
@@ -19,95 +19,63 @@ import static org.junit.jupiter.api.Assertions.*;
  * and receiving updates from the Scheduler.
  */
 public class DroneTest {
-    private static Drone drone;
-    private static InetAddress droneAddr;
-    private static int dronePort;
 
-    /**
-     * Sets up the test environment before each test case.
-     * Initializes a new instance of the Scheduler and Drone classes.
-     */
-    @BeforeAll
-    public static void setUp() {
-        drone = new Drone(6000);
-    }
-
-//    @Test
-//    public void testSendWakeupMessage(){
-//        new Thread(() -> {
-//            try {
-//                DatagramSocket testSocket = new DatagramSocket(6000);
-//                DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
-//
-//                testSocket.receive(receivePacket);
-//                String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
-//                assertTrue(receivedMessage.equals("ONLINE"));
-//                System.out.println("Expected: ONLINE, Actual: " +receivedMessage);
-//
-//                String sendMessage = "RECEIVED";
-//                DatagramPacket sendPacket = new DatagramPacket(sendMessage.getBytes(), sendMessage.getBytes().length, receivePacket.getAddress(), receivePacket.getPort());
-//                testSocket.send(sendPacket);
-//
-//                this.droneAddr = receivePacket.getAddress();
-//                this.dronePort = receivePacket.getPort();
-//
-//                testSocket.close();
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }).start();
-//
-//        drone.sendWakeupMessage();
-//    }
-
-    /**
-     * Tests the functionality of sending a fire request to the Scheduler
-     * and verifying that the update is received after the fire is dealt with.
-     *
-     * @throws InterruptedException if the thread is interrupted while waiting.
-     */
     @Test
-    public void testSendToFire() throws InterruptedException {
-        Zone zone = new Zone (1, 0,0,700,600);
-        Event fireEvent = new Event(LocalTime.now(), zone, Event.Type.FIRE_DETECTED, Event.Severity.LOW);
+    public void testRun(){
+        Drone drone = new Drone(6000);
 
         new Thread(() -> {
             try {
                 DatagramSocket testSocket = new DatagramSocket(6000);
-
                 DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
+
                 testSocket.receive(receivePacket);
-                this.droneAddr = receivePacket.getAddress();
-                this.dronePort = receivePacket.getPort();
+                String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                assertTrue(receivedMessage.equals("ONLINE"));
+                System.out.println("\nExpected: ONLINE, Actual: " +receivedMessage+ "\n");
 
-                assertTrue(drone.getStateAsString().equals("Idle"));
-                System.out.println("State: Expected: Idle, Actual: " +drone.getStateAsString());
+                InetAddress droneAddr = receivePacket.getAddress();
+                int dronePort = receivePacket.getPort();
 
-                byte[] buffer = fireEvent.serializeEvent();
-                DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, this.droneAddr, this.dronePort);
+                String sendMessage = "RECEIVED";
+                DatagramPacket sendPacket = new DatagramPacket(sendMessage.getBytes(), sendMessage.getBytes().length, droneAddr, dronePort);
                 testSocket.send(sendPacket);
+
+                Zone zone = new Zone (1, 0,0,700,600);
+                Event event = new Event(LocalTime.now(), zone, Event.Type.FIRE_DETECTED, Event.Severity.LOW);
+
+                byte[] buffer = event.serializeEvent();
+                DatagramPacket sendPacket2 = new DatagramPacket(buffer, buffer.length, droneAddr, dronePort);
+                testSocket.send(sendPacket2);
+
+                DatagramPacket receivePacket2 = new DatagramPacket(new byte[1024], 1024);
+
+                testSocket.receive(receivePacket2);
+                DroneResponse droneResponse = DroneResponse.deserializeResponse(receivePacket2.getData());
+                Event returnedEvent = droneResponse.getEvent();
+                System.out.println("\nEvent Severity: Expected: OUT, Actual: " +returnedEvent.getSeverity()+ "\n");
+
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }).start();
 
+        //Gives the other thread some time to start
+        try {
+            sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         drone.start();
-//
-//        assertTrue(drone.getStateAsString().equals("Idle"));
-//        System.out.println("State: Expected: Idle, Actual: " +drone.getStateAsString());
-//        drone.checkMessage();
-//        assertTrue(drone.getStateAsString().equals("En Route"));
-//        System.out.println("State: Expected: En Route, Actual: " +drone.getStateAsString());
 
-        while(true){
-
-            System.out.println("State: Expected: Idle, Actual: " +drone.getStateAsString());
-            try {
-                sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        //Lets the threads run
+        try {
+            sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
+
 }
